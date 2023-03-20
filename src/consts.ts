29dfdc293d6d7bc615a53609fe959bf2e5155c0e
onebarloop <alexanderbayer@posteo.de>
2023-03-20 17:42:45 +0100

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,17 @@
+export const PICTURES = [
+  {
+    src: '/pics/image00001.jpeg',
+    width: 3464,
+    height: 3464,
+  },
+  {
+    src: '/pics/image00002.jpeg',
+    width: 2828,
+    height: 4096,
+  },
+  {
+    src: '/pics/image00003.jpeg',
+    width: 2848,
+    height: 4096,
+  },
+];
