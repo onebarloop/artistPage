94343832cd05aa59af4eb2c5591bcf019e648052
onebarloop <alexanderbayer@posteo.de>
2023-03-24 17:38:14 +0100

    styled form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -20,20 +20,45 @@ export default function Form() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
+    <form
+      onSubmit={handleSubmit}
+      class='md:w-2/5 font-sono bg-myYellow text-black font-bold mt-8 flex flex-col gap-2 p-4'
+    >
+      <label for='name' class=''>
         Name
-        <input type='text' id='name' name='name' required />
       </label>
-      <label>
+      <input
+        type='text'
+        id='name'
+        name='name'
+        required
+        class='bg-black text-myYellow p-2 '
+      />
+      <label for='email' class=''>
         Email
-        <input type='email' id='email' name='email' required />
       </label>
-      <label>
-        Message
-        <textarea id='text' name='text' required />
-      </label>
-      <button type='submit'>Send</button>
+      <input
+        type='email'
+        id='email'
+        name='email'
+        required
+        class='bg-black text-myYellow p-2'
+      />
+      <label for='text'>Message</label>
+      <textarea
+        id='text'
+        name='text'
+        rows={4}
+        required
+        class='bg-black text-myYellow p-2'
+        placeholder='leave me a message'
+      />
+      <button
+        type='submit'
+        class='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow opacity-60 active:scale-95 hover:opacity-70  transition ease-in-out mt-2'
+      >
+        Send
+      </button>
       {responseMessage && <p>{responseMessage}</p>}
     </form>
   );
