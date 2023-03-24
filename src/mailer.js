7562c460f0612768dce3639596d1e0271c1bc042
onebarloop <alexanderbayer@posteo.de>
2023-03-24 09:46:15 +0100

    Mail sends body

--- src/mailer.js
+++ src/mailer.js
@@ -1,36 +1,36 @@
 'use strict';
 import nodemailer from 'nodemailer';
 
-// async..await is not allowed in global scope, must use a wrapper
-export async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
+const mailHost = import.meta.env.EMAIL_HOST;
+const mailUser = import.meta.env.EMAIL_USER;
+const mailPass = import.meta.env.EMAIL_PASSWORD;
 
-  // create reusable transporter object using the default SMTP transport
+export async function main(recipient, text) {
+  // let testAccount = await nodemailer.createTestAccount();
+
   let transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
+    host: mailHost,
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
+      user: mailUser, // generated ethereal user
+      pass: mailPass, // generated ethereal password
     },
   });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: 'alexanderbayer@posteo.de', // list of receivers
+    from: mailUser, // sender address
+    to: recipient, // list of receivers
     subject: 'Hello ✔', // Subject line
-    text: 'Hello world?', // plain text body
-    html: '<b>Hello world?</b>', // html body
+    text: text, // plain text body
+    html: `<b>${text}</b>`, // html body
   });
 
   console.log('Message sent: %s', info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   // Preview only available when sending through an Ethereal account
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
--- src/pages/mail.ts
+++ src/pages/mail.ts
@@ -1,12 +1,48 @@
 import type { APIRoute } from 'astro';
-//@ts-ignore
-import { main } from '../mailer';
 
-export const post: APIRoute = ({ request }) => {
-  main();
-  return {
-    body: JSON.stringify({
-      message: 'This was a POST!',
-    }),
-  };
+import nodemailer from 'nodemailer';
+
+const mailHost = import.meta.env.EMAIL_HOST;
+const mailUser = import.meta.env.EMAIL_USER;
+const mailPass = import.meta.env.EMAIL_PASSWORD;
+
+//Mail-Function
+async function mail(text: string) {
+  let transporter = nodemailer.createTransport({
+    host: mailHost,
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: mailUser,
+      pass: mailPass,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: mailUser,
+    to: 'XXX',
+    subject: 'Hello ✔',
+    text: text,
+    html: `<b>${text}</b>`,
+  });
+
+  console.log('Message sent: %s', info);
+}
+
+// API-Route
+export const post: APIRoute = async ({ request }) => {
+  if (request.headers.get('Content-Type') === 'application/json') {
+    const body = await request.json();
+    const text = body.text;
+    mail(text);
+    return new Response(
+      JSON.stringify({
+        message: 'Danke für die Nachricht',
+      }),
+      {
+        status: 200,
+      }
+    );
+  }
+  return new Response(null, { status: 400 });
 };
